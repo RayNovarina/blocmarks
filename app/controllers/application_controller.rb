6ab65ef67bd1e2ec0530c53f67d379c18b9907a2
Ray <RNova94037@Gmail.com>
2016-02-01 22:15:58 -0800

    SignUp: Add Devise for Authentication, Mailcatcher to verify emails are sent in dev.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Via Devise: redirect the user to the login page if they're not signed in:
+  before_action :authenticate_user!
 end

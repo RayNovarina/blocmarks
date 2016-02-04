457a763f3caf1face9fa9e2a51fb7e040ffe34ef
Ray <RNova94037@Gmail.com>
2016-02-03 23:46:51 -0800

    2-Sign-up: Added styling for devise reg sign-up page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,15 @@ module ApplicationHelper
   #     <% page_header "Sign Up" %>
   #     Gets inserted into the .erb page via /views/layouts/application.html.erb
   #       <h1><%= yield :page_header %></h1>
-  def page_header(text)
-    content_for(:page_header) { text.to_s }
+  def page_title(text)
+    content_for(:page_title) { text.to_s }
+  end
+
+  def page_main(text)
+    content_for(:page_main_body_content_at_last) { text.to_s }
+  end
+
+  def page_main_body_content_at_last(text)
+    content_for(:page_main_body_content_at_last) { text.to_s }
   end
 end

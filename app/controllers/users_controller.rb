1ce7ec872d0e8118dbecf1d10ff6b4ec47c1c188
Ray <RNova94037@Gmail.com>
2016-02-14 08:24:32 -0800

    [WIP]9.user-profiles: Fix heroku postgres sql syntax err. Select for DISTINCT must be first.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
     # populate @user_bookmarks with the user's bookmarks
     # @user_bookmarks = Bookmark.where(user_id: current_user.id).group(:topic_id)
 
-    @topics_with_user_bookmarks = Topic.all.joins(:bookmarks).where('bookmarks.user_id = ?', current_user.id).reorder('lower(topics.title) ASC').select('topics.*').distinct('topics.id')
+    @topics_with_user_bookmarks =
+      Topic.select('topics.*')
+           .joins(:bookmarks)
+           .where('bookmarks.user_id = ?', current_user.id)
+           .reorder('lower(topics.title) ASC')
+           .distinct('topics.id')
 
     # @user_bookmarks_for_first_topic =
     #  @topics_with_user_bookmarks[0].bookmarks = topic.bookmarks.where('bookmarks.user_id = ?', :current_user.id)
@@ -21,7 +26,12 @@ class UsersController < ApplicationController
     # populate @liked_bookmarks with liked bookmarks
     # @liked_bookmarks = Bookmark.joins(:likes).where('likes.user_id = ?', current_user.id).group(:topic_id)
 
-    @topics_with_user_liked_bookmarks = Topic.all.joins(bookmarks: :likes).where('likes.user_id = ?', current_user.id).reorder('lower(topics.title) ASC').select('topics.*').distinct('topics.id')
+    @topics_with_user_liked_bookmarks =
+      Topic.select('topics.*')
+           .joins(bookmarks: :likes)
+           .where('likes.user_id = ?', current_user.id)
+           .reorder('lower(topics.title) ASC')
+           .distinct('topics.id')
 
     # @liked_bookmarks_for_first_topic =
     #  @topics_with_user_liked_bookmarks[0].bookmarks.where('bookmarks.user_id = ?', :current_user.id)

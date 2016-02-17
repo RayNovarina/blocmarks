26302713c5cb45f451dec23ed21f253edca91306
Ray <RNova94037@Gmail.com>
2016-02-17 12:35:10 -0800

    [WIP]extra-credit-embedly: Fixed like/unlike policy. User can like their own bookmarks.

--- app/policies/like_policy.rb
+++ app/policies/like_policy.rb
@@ -1,3 +1,13 @@
 #
 class LikePolicy < ApplicationPolicy
+  def create?
+    #user.present? && ((defined? record.user).nil? ? true : (record.user.nil? ? true : !(record.user == user)))
+    return false unless user.present?
+    return true  if (defined? record.user).nil? || record.user.nil?
+    record.bookmark.user == user ? false : true
+  end
+
+  def destroy?
+    create?
+  end
 end
